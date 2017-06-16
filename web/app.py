b1bc55f1323b1ab8c88921b1c50ba80a9d4be1f3
PatrickVienne <polyun.p@gmail.com>
2017-06-16 15:38:53 +0200

    add mail lsitening on post 1025

--- web/app.py
+++ web/app.py
@@ -13,30 +13,6 @@ template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
 
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:my-secret-pw@192.168.0.88:3306/test_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = flask_sqlalchemy.SQLAlchemy(app)
-
-class UsersPy(db.Model):
-    __tablename__ = "userspy"
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return '<title {}'.format(self.username)
-
-
-db.init_app(app)
-
-db.create_all()
-
 
 @app.before_first_request
 def setup_logging():
@@ -44,38 +20,6 @@ def setup_logging():
         # In production mode, add log handler to sys.stderr.
         app.logger.addHandler(logging.StreamHandler())
         app.logger.setLevel(logging.INFO)
-
-
-@app.route('/test_entry_db/')
-def test_entry_db():
-    admin = UsersPy('user1', 'password1')
-    guest = UsersPy('user2', 'password2')
-
-    db.session.add(admin)
-    db.session.add(guest)
-
-    # db.session.merge(admin)
-    # db.session.merge(guest)
-    db.session.commit()
-
-    results = UsersPy.query.all()
-    json_results = []
-    for result in results:
-        d = {'username': result.username,
-             'password': result.password}
-        json_results.append(d)
-
-    return flask.jsonify(items=json_results)
-
-
-@app.route('/testdb')
-def testdb():
-    logging.info('Test Database query')
-    print "testing Database Connection"
-    if db.session.query("1").from_statement("SELECT 1").all():
-        return 'It works.'
-    else:
-        return 'Something izs broken.'
 
 
 @app.route('/')
