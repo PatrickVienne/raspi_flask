96f71d3449e81fdc1d84ef8fa605d2a8483c5a83
PatrickVienne <polyun.p@gmail.com>
2017-06-16 12:27:37 +0200

    reformat app.py

--- web/app.py
+++ web/app.py
@@ -5,11 +5,11 @@ import jinja2
 import json
 import logging
 
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='example.log', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='example.log',
+                    level=logging.DEBUG)
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
-
 
 app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:my-secret-pw@192.168.0.88:3306/test_db'
@@ -17,20 +17,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = flask_sqlalchemy.SQLAlchemy(app)
 
+
 class UsersPy(db.Model):
+    __tablename__ = "userspy"
 
-  __tablename__ = "userspy"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(255), nullable=False)
-  password = db.Column(db.String(255), nullable=False)
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-  def __init__(self, username, password):
-    self.username = username
-    self.password = password
+    def __repr__(self):
+        return '<title {}'.format(self.username)
 
-  def __repr__(self):
-    return '<title {}'.format(self.username)
 
 db.init_app(app)
 
@@ -53,19 +54,20 @@ def test_entry_db():
     db.session.add(admin)
     db.session.add(guest)
 
-    #db.session.merge(admin)
-    #db.session.merge(guest)
+    # db.session.merge(admin)
+    # db.session.merge(guest)
     db.session.commit()
 
     results = UsersPy.query.all()
     print db.table
     json_results = []
     for result in results:
-      d = {'username': result.username,
-           'password': result.password}
-      json_results.append(d)
+        d = {'username': result.username,
+             'password': result.password}
+        json_results.append(d)
 
     return flask.jsonify(items=json_results)
+
 
 @app.route('/testdb')
 def testdb():
@@ -81,7 +83,7 @@ def testdb():
 def hello_world():
     logging.info("received request")
     template = jinja_env.get_template("index.html")
-    params={}
+    params = {}
     return template.render(params)
 
 
@@ -89,17 +91,17 @@ def hello_world():
 def on_push():
     template = jinja_env.get_template("index.html")
     print "received github push"
-    print "*"*20
+    print "*" * 20
     print flask.request
     print "*" * 20
     print flask.request.__dict__
     print "*" * 20
-    params={}
+    params = {}
     logging.debug('{}'.format(flask.request.__dict__))
     logging.info('Info githubhook')
     logging.warning('Warning githubhook')
     return template.render(params)
 
+
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=80)
-
